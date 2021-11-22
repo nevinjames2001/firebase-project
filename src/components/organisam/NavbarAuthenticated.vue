a2f80539644cfaefb4383d776c7ffd438422848e
nevinjames2001 <nevinjames2001@gmail.com>
2021-11-21 21:56:43 -0500

    final

--- src/components/organisam/NavbarAuthenticated.vue
+++ src/components/organisam/NavbarAuthenticated.vue
@@ -2,26 +2,42 @@
     <Navbar>
         <template v-slot:menuitems>
             <NavItem text="logout" to="/" userouterlink="false" @click="logout">Logout</NavItem>
+            <div id='usericon'></div>
             </template>
     </Navbar>
 </template>
 <script>
-import {defineAsyncComponent} from 'vue'
+import {defineAsyncComponent,onMounted,computed} from 'vue'
 import {useRouter} from 'vue-router'
 import {useStore} from 'vuex'
 import {getAuth,signOut} from 'firebase/auth'
-const Navbar= defineAsyncComponent(()=>import(/*webpackChunkName:"Navbar"*/'@/components/molecules/Navbar.vue'))
+import {userinitial} from 'adi-userinitials'
+import Navbar from '@/components/molecules/Navbar.vue'
 const NavItem= defineAsyncComponent(()=>import(/*webpackChunkName:"NavItem"*/'@/components/molecules/NavItem.vue'))
 export default{
     components:{
         Navbar,
        NavItem,
+
     },
     setup()
     {
         const router=useRouter()
         const store=useStore()
         const auth=getAuth()
+        const email=computed(()=>store.getters['user/getEmail'])
+
+        onMounted(()=>{
+            userinitial({
+                root:'#usericon',
+                height:'50px',
+                width:'50px',
+                backGround:'red',
+                fontColor:'white',
+                name:email.value
+            })
+
+        })
         function logout(){
 
             signOut(auth).then(() => {
